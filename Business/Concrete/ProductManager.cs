88d323aaf2585c8515f2ae0a3760fe2fb7e3fc81
MX <MX@DESKTOP-PQB3BOA>
2021-02-13 23:55:37 +0300

    13 Şubat 2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -41,7 +41,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==13)
+            if (DateTime.Now.Hour==1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
 
